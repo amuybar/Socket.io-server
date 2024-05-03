a3f94137ec03f40b76b40068d2ec37e59b81af29
Barrack Amuyunzu <amuyunzubq@gmail.com>
2024-05-03 05:43:25 +0300

    Created a simple Server that handle socket connection to the frontend It creates a connection and emits messages to a spesific rooms which user can join

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const express=require('express');
 const app=express();
-
 const http=require('http');
-const {Server}=require('socket.io')
 const cors=require('cors');
+const {Server}=require('socket.io')
 
-app.use(cors())
+
+app.use(cors());
+
 const server=http.createServer(app);
 
 const io=new Server(server,{
   cors:{
-    origin:'http://localhost:3001',
+    origin:'http://localhost:5173',
     methods:['GET','POST']
   }
 });
 
-io.on("connection",(socket)=>{
-  console.log("New user connected");
-  socket.on("disconnect",()=>{
-    console.log("user disconnected");
-  })
+io.on('connection',(socket)=>{
+ console.log(socket.id);
+
+ socket.on('join_room',(data)=>{
+   socket.join(data)
+   console.log(`User with ID ${socket.id} joined room ${data}`);
+ })
+
+ socket.on('send_message',(data)=>{
+  socket.to(data.room).emit("receive_message",data)
+ })
+
+ socket.on('disconnect',(socket)=>{
+   console.log("User Disconnected", socket.id)
+ })
 })
 
-server.listen(3001,()=>{
+
+
+server.listen(3001, ()=>{
   console.log('server is running on port 3001');
 })
